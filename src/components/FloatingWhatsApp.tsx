4453435a2ab00b2a5a09700481948ed37326a947
vendasjung <vendas1@opticajung.com.br>
2026-08-14 19:44:54 -0300

    Update FloatingWhatsApp.tsx

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -2,7 +2,7 @@ import { MessageCircle } from "lucide-react";
 
 const FloatingWhatsApp = () => {
   const whatsappNumber = "5549988044635";
-  const whatsappLink = `https://wa.me/${whatsappNumber}?text=Olá! Gostaria de solicitar um orçamento de óculos da Ótica Visão.`;
+  const whatsappLink = `https://wa.me/${whatsappNumber}?text=Olá! Vim pelo site e gostaria de solicitar um orçamento de óculos da Ótica Visão.`;
 
   return (
     <a
