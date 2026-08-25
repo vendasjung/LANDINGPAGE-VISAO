8193f3766af943f4262425a9b638a771e1b0cd1b
vendasjung <vendas1@opticajung.com.br>
2026-08-25 14:56:55 -0300

    Add VisiofficeSection and MapSection to Index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,9 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import ServicesSection from "@/components/ServicesSection";
+import VisiofficeSection from "@/components/VisiofficeSection";
 import BenefitsSection from "@/components/BenefitsSection";
+import MapSection from "@/components/MapSection";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
@@ -13,7 +15,9 @@ const Index = () => {
       <main>
         <HeroSection />
         <ServicesSection />
+        <VisiofficeSection />
         <BenefitsSection />
+        <MapSection />
         <CTASection />
       </main>
       <Footer />
